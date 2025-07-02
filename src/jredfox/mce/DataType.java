7f7d7a961f67b9b56a9941227d790d27c13f12e8
jredfox <jredfox_@outlook.com>
2025-07-02 14:47:55 -0500

    backup

--- src/jredfox/mce/DataType.java
+++ src/jredfox/mce/DataType.java
@@ -1,23 +1,44 @@
 package jredfox.mce;
 
+import org.objectweb.asm.Opcodes;
+
 public enum DataType {
 	
-	BOOLEAN,
-	BYTE,
-	SHORT,
-	INT,
-	LONG,
-	FLOAT,
-	DOUBLE,
-	STRING,
-	WRAPPED_BOOLEAN,
-	WRAPPED_BYTE,
-	WRAPPED_SHORT,
-	WRAPPED_INT,
-	WRAPPED_LONG,
-	WRAPPED_FLOAT,
-	WRAPPED_DOUBLE,
-	NULL;
+	BOOLEAN("Z", "", "", Opcodes.T_BOOLEAN, Opcodes.BASTORE, false),
+	BYTE("B", "", "", Opcodes.T_BYTE, Opcodes.BASTORE, false),
+	SHORT("S", "", "", Opcodes.T_SHORT, Opcodes.SASTORE, false),
+	INT("I", "", "", Opcodes.T_INT, Opcodes.IASTORE, false),
+	LONG("J", "", "", Opcodes.T_LONG, Opcodes.LASTORE, false),
+	FLOAT("F", "", "", Opcodes.T_FLOAT, Opcodes.FASTORE, false),
+	DOUBLE("D", "", "", Opcodes.T_DOUBLE, Opcodes.DASTORE, false),
+	STRING("Ljava/lang/String;", "java/lang/String", "", -1, Opcodes.AASTORE, true),
+	WRAPPED_BOOLEAN("Ljava/lang/Boolean;", "java/lang/Boolean", "(Z)Ljava/lang/Boolean;", -1, Opcodes.AASTORE, true),
+	WRAPPED_BYTE("Ljava/lang/Byte;", "java/lang/Byte", "(B)Ljava/lang/Byte;", -1, Opcodes.AASTORE, true),
+	WRAPPED_SHORT("Ljava/lang/Short;", "java/lang/Short", "(S)Ljava/lang/Short;", -1, Opcodes.AASTORE, true),
+	WRAPPED_INT("Ljava/lang/Integer;", "java/lang/Integer", "(I)Ljava/lang/Integer;", -1, Opcodes.AASTORE, true),
+	WRAPPED_LONG("Ljava/lang/Long;", "java/lang/Long", "(J)Ljava/lang/Long;", -1, Opcodes.AASTORE, true),
+	WRAPPED_FLOAT("Ljava/lang/Float;", "java/lang/Float", "(F)Ljava/lang/Float;", -1, Opcodes.AASTORE, true),
+	WRAPPED_DOUBLE("Ljava/lang/Double;", "java/lang/Double", "(D)Ljava/lang/Double;", -1, Opcodes.AASTORE, true),
+	NULL(null, null, null, -1, -1, false);
+	
+	public final String desc;
+	public final String clazz;
+	public final String valueOfDesc;
+	public final int newArrayType;
+	public final int arrayStore;
+	public final boolean isWrapper;
+	public final boolean isObject;
+	
+	private DataType(String desc, String clazz, String valueOfDesc, int newArrType, int store, boolean isObject)
+	{
+		this.desc = desc;
+		this.clazz = clazz;
+		this.valueOfDesc = valueOfDesc;
+		this.newArrayType = newArrType;
+		this.arrayStore = store;
+		this.isWrapper = valueOfDesc != null && !valueOfDesc.isEmpty();
+		this.isObject = isObject;
+	}
 	
 	/**
 	 * Get's the Primitive form of the Type

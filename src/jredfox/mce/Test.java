d42020f1fb715a669bf7c9b8034ed62fcc98b26a
jredfox <jredfox_@outlook.com>
2025-07-03 12:05:33 -0500

    Update Test.java

--- src/jredfox/mce/Test.java
+++ src/jredfox/mce/Test.java
@@ -32,6 +32,7 @@ public class Test {
 	private static double[] arr_double = 	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	private static String[] arr_str = 		{"Entity Id", "Index 1", "Index 2", "Index 3", "Index 4", "Index 5", "Index 6", "Index 7", "Index 8", "Index 9"};
 	private static boolean[] arr_tst = 	{false, false, true, false, false, false};
+	private static char[] arr_char = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 	
 	//Start static array wrappers
 	private static Boolean[] o_arr_bool = 	{false, false, false, false, false, false};
@@ -42,25 +43,10 @@ public class Test {
 	private static Long[] o_arr_long = 		{0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L};
 	private static Float[] o_arr_float = 	{0F, 1F, 2F, 3F, 4F, 5F, 6F, 7F, 8F, 9F, 10F, 11F, 12F, 13F, 14F, 15F, 16F, 17F, 18F, 19F, 20F};
 	private static Double[] o_arr_double = 	{0D, 1D, 2D, 3D, 4D, 5D, 6D, 7D, 8D, 9D, 10D, 11D, 12D, 13D, 14D, 15D, 16D, 17D, 18D, 19D, 20D};
+	private static Character[] o_arr_char = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
 	
 	static
 	{
-//		id_c = (char) -1;
-//		id_c = 0;
-//		id_c = 1;
-//		id_c = 2;
-//		id_c = 3;
-//		id_c = 4;
-//		id_c = 5;
-//		id_c = 6;
-//		id_c = 128;
-//		id_c = 'a';
-//		id_c = 'z';
-//		id_c = '!';
-//		id_c = Character.MAX_VALUE;
-//		id_c = '0';
-//		id_c = Short.MAX_VALUE;
-//		char[] c = new char[] {'a', 'b'};
 //		Character[] ca = new Character[] {'a', 'b'};
 	}
 	
@@ -85,7 +71,8 @@ public class Test {
 		System.out.println("id_l:" +   id_l);
 		System.out.println("id_str:" + id_str);
 		
-		ArrUtils.print(arr_str);
+		ArrUtils.print(arr_char);
+		ArrUtils.print(o_arr_char);
 	}
 
 	public static void load() {
